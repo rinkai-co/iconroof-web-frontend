c8f538ffcd570cf20413eeb456261ded83dfbc56
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-20 17:54:14 +0700

    feat: add metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,52 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>จำหน่ายอุปกรณ์ระแนงไวนิล | ไม้ระแนงเฌอร่า-ระแนงไม้เทียม – Iconroof</title>
+	<meta
+		name="description"
+		content="จำหน่ายและติดตั้งระแนงบังตาไวนิล AMIGO ระแนงชิด ระแนงเว้นร่อง ระแนงบานเกล็ด โดยไอคอนรูฟ ขายปลีก-ส่ง ราคาโรงงาน ทันสมัย แข็งแรง สวยงาม ระแนงบังตาไวนิล รับประกัน 10 ปี อบสีด้วย พาวเดอร์ โค้ท ตั้งแต่ 08.00-17.30 น. เปิดบริการวันจันทร์-เสาร์ บริการ: รับติดตั้งระแนงไวนิล, เปิดบริการ จันทร์-ศุกร์, ตั้งแต่ 08.00-17.30 น."
+	/>
+	<meta
+		name="keywords"
+		content="amigo, ระแนงบังตา, ระแนงบังตาไวนิล, ระแนงบังตาไวนิล AMIGO, upvc, Amigo, scg ระแนงชิด, ระแนงเว้นร่อง, ระแนงบานเกล็ด, ระแนงบังแดด, ระแนงตกแต่งบ้าน, ระแนงระเบียง, ระแนงกันแดด, ระแนงบังสายตา, ระแนงหน้าบ้าน, ระแนงข้างบ้าน, ระแนงแต่งบ้าน, ระแนงแต่งสวน, ไวนิลบังตา, แผ่นไวนิล, ไวนิลตกแต่ง, ไวนิลภายนอก, ไวนิลภายใน, วัสดุตกแต่งบ้าน, วัสดุก่อสร้าง, หลังคาไวนิล, หลังคาไวนิล AMIGO, ไวนิลคุณภาพสูง, ไวนิลราคาถูก, วัสดุตกแต่งภายนอก, แผ่นไวนิลทนแดด, แผ่นไวนิลทนน้ำ, ไวนิลตกแต่งผนัง, ผนังไวนิล, ไวนิลตกแต่งภายนอก, ติดตั้งระแนง, ช่างติดตั้งระแนง, ระแนงสำเร็จรูป, ผลิตภัณฑ์ไวนิล, ไวนิลกันน้ำ, ไวนิลกันปลวก, วัสดุกันความร้อน, ไวนิลทนฝน, วัสดุก่อสร้างบ้าน, สร้างบ้าน, รีโนเวทบ้าน, บ้านสวยด้วยไวนิล, ไอคอนรูฟ, Iconroof, ไวนิล Amigo, ระแนง, ระ แนง, ระแน ง, บังตา, ฝ้า, ไอคอนรุฟ"
+	/>
+	<meta
+		property="og:title"
+		content="จำหน่ายอุปกรณ์ระแนงไวนิล | ไม้ระแนงเฌอร่า-ระแนงไม้เทียม – Iconroof"
+	/>
+	<meta
+		property="og:description"
+		content="บริการจำหน่ายและติดตั้งระแนงบังตาไวนิล AMIGO ระแนงชิด ระแนงเว้นร่อง ระแนงบานเกล็ด โดยไอคอนรูฟ ขายปลีก-ส่ง ราคาโรงงาน ทันสมัย แข็งแรง สวยงาม ระแนงบังตาไวนิล รับประกัน 10 ปี อบสีด้วย พาวเดอร์ โค้ท ตั้งแต่ 08.00-17.30 น. เปิดบริการวันจันทร์-เสาร์ บริการ: รับติดตั้งระแนงไวนิล, เปิดบริการ จันทร์-ศุกร์, ตั้งแต่ 08.00-17.30 น."
+	/>
+	<meta property="og:url" content="https://iconroof.co.th/" />
+	<meta property="og:site_name" content="Next.js" />
+	<meta
+		property="og:image"
+		content="https://tvss01.iconroof.co.th/e6c6f6e8-e73e-40b1-9f05-5436e5f99137/asset/og-img.jpg"
+	/>
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:image:alt" content="iconroof ระแนง" />
+	<meta property="og:locale" content="th_TH" />
+	<meta property="og:type" content="website" />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@300;400;500;600;700&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 {@render children()}
+
+<footer class="flex min-h-fit w-full flex-col items-center justify-center gap-4 px-12 font-sans">
+	<img src="https://www.rinkai.co/images/logo-b.svg" alt="Icon" class="h-8 w-fit" />
+	<div class="gap2 flex flex-col items-center text-neutral-700">
+		<a href="https://www.rinkai.co/" class="underline decoration-2 underline-offset-4"
+			>© 2025 Rinkai Industries</a
+		>
+	</div>
+	<div class="flex flex-col rounded-lg bg-neutral-100 p-2.5 text-neutral-700">
+		<code>Staging Environment</code>
+	</div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import iconOriginal from '$lib/assets/logoOriginal.svg';
+</script>
+
+<section
+	class="flex min-h-screen w-full flex-col items-center justify-center gap-4 px-12 font-sans"
+>
+	<img src={iconOriginal} alt="Icon" class="h-12 w-fit" />
+	<div class="gap2 flex flex-col items-center text-neutral-700">
+		<h1 class="text-2xl font-medium">เว็บไซต์ Staging Engine</h1>
+		<p class="text-lg">
+			<a href="https://iconroof.co.th/" class="underline decoration-2 underline-offset-2"
+				>คลิกที่นี่</a
+			> เพื่อเข้าใช้งานเว็บไซต์หลัก
+		</p>
+	</div>
+	<div class="flex flex-col rounded-lg bg-neutral-100 p-2.5 text-neutral-700">
+		<code>&gt; sveltekit</code>
+		<code>&gt; southeastasia-02.iconroof.co.th</code>
+		<code>&gt; @rinkai.co</code>
+	</div>
+</section>
