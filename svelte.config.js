c02a4f53bf106ad53c81a53e0f210c9b83e39dd0
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-12-07 22:35:52 +0700

    add adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: isVercel ? adapterVercel() : adapterNode()
+	}
 };
 
 export default config;
